820d4da896b9928a8cec5805f4e1f65299245a6b
PeterDuck <fallbarn@hotmail.com>
2020-04-28 08:56:21 +0100

    Yet more comment

--- OdeToFood/OdeToFood.Core/Restaurant.cs
+++ OdeToFood/OdeToFood.Core/Restaurant.cs
@@ -2,10 +2,14 @@
 
 namespace OdeToFood.Core
 {
+
+    // sle note: define a class to represent a restaurant which will
+    // be mapped to a database table by EF.
     public class Restaurant 
     {
         public int Id { get; set; }
 
+        // SLE note: works inconjunction with binding to verifyinput data.
         [Required, StringLength(80)]
         public string Name { get; set; }
 
--- OdeToFood/OdeToFood.Data/OdeToFoodDbContext.cs
+++ OdeToFood/OdeToFood.Data/OdeToFoodDbContext.cs
@@ -6,14 +6,17 @@ using OdeToFood.Core;
 
 namespace OdeToFood.Data
 {
+    // sle note: main gateway into the database and all tables and other connecting objects.
     public class OdeToFoodDbContext : DbContext
     {
+        // sle note: Inject the Configuration string (dependency injection).
         public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options)
             : base(options)
         {
                 
         }
 
+        // sle note: represents a single table in the database.
         public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
